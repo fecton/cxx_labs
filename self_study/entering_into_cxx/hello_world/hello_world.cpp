4370ab942ce9727d61a3df8aa6f7707acd19ecb1
fecton <bopongamer@gmail.com>
2022-01-05 21:39:04 +0200

    Initial commit

---
+++ self_study/entering_into_cxx/hello_world/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "version.cpp"
+//  $ g++ hello_world.cpp -o hello -std=c++14
+int main(int argc, char* argv[]){
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Version: " << test::getVersion() << std::endl;
+
+    return 0;
+}
+
